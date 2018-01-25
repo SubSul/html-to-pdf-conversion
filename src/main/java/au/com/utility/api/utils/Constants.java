7751ceb1dbdcea61af0939a7fdf34d80e3d14060
Subhash Sulanthar <subhash.s1987@gmail.com>
2018-01-25 12:29:58 +1100

    Add constants class.

---
+++ src/main/java/au/com/utility/api/utils/Constants.java
@@ -0,0 +1,21 @@
+package au.com.utility.api.utils;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String UTF_8 = "UTF-8";
+    public static final String FTL_TEMPLATE = "ftlTemplate";
+    public static final String FTL = ".ftl";
+    public static final String PDF = ".pdf";
+    public static final String FORWARD_SLASH = "/";
+    public static final int OK = 200;
+
+    public static final String OPTIONS = "OPTIONS";
+    public static final String POST = "POST";
+
+    public static final String EMPTY = "";
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int BAD_REQUEST = 400;
+}
